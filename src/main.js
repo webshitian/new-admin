835bd0ff49ecbca519fd4038df18a065d31c0af0
webshitian <1047221702@qq.com>
2019-10-07 19:59:47 +0800

    5-登录成功

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import App from './App.vue'
 import router from './router'
 //导入element-ui
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from "axios"
+
 
 // 可以忽略，生产环境时候是否有提示
 Vue.config.productionTip = false
-
+//把axios挂载到vue的原型
+Vue.prototype.$axios = axios;
+//设置基准路径
+axios.defaults.baseURL = "http://localhost:3000";
 // 注册element插件
 Vue.use(ElementUI);
 
