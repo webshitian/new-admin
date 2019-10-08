99439863cafadadf1942787a4d2fc123318fd06a
webshitian <1047221702@qq.com>
2019-10-08 14:15:16 +0800

    9-面包屑功能

--- src/router.js
+++ src/router.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 
 import Login from '@/views/Login'
 import Index from '@/views/Index'
+import PostList from '@/views/PostList';
+import PostAdd from '@/views/PostAdd';
+
 //注册路由插件
 Vue.use(Router)
 
@@ -20,8 +23,13 @@ export default new Router({
     //登录页
     {path:"/login",component:Login},
     //后台首页
-    {path:"/",component:Index,Index,children:[
-      
+    {
+      path:"/",
+      component:Index,
+      meta:"首页",
+      children:[
+        {path:"post_list",component:PostList,meta:"文章列表"},
+        {path:"post_add",component:PostAdd,meta:"发布文章"}
     ]}
   ]
 })
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,7 +3,7 @@
         <!-- 侧边栏 -->
         <el-aside width="200px">
             <div class="logo">
-                黑马头条
+                新浪新闻
             </div>
 
             <!-- default-active：当前高亮选中的菜单 -->
@@ -22,10 +22,10 @@
                         <span>文章管理</span>
                     </template>
                     <el-menu-item index="1-1">
-                        文章列表
+                        <router-link to="/post_list">文章列表</router-link>
                     </el-menu-item>
                     <el-menu-item index="1-2">
-                        发布文章
+                        <router-link to="/post_add">发布文章</router-link>
                     </el-menu-item>
                 </el-submenu>
             </el-menu>
@@ -39,7 +39,11 @@
                 <span>退出</span>
             </el-header>
             <!-- 子页面显示的内容 -->
-            <el-main>Main</el-main>
+            <el-main>
+                <div>{{breaks}}</div>
+                <!--显示子路由匹配的页面-->
+                <router-view></router-view>
+            </el-main>
         </el-container>
     </el-container>
 </template>
@@ -49,6 +53,33 @@ export default {
      data(){
         return {
             user: JSON.parse(localStorage.getItem("user") || `{}` )
+        }
+    },
+    //监听url地址的变化，watch页面以加载时候不会执行，只有等到url变化时候才会执行
+    // watch:{
+    //     $route(){
+    //         const {matched} = this.$route;
+    //         const arr = [];
+            
+    //         matched.forEach(v => {
+    //             arr.push(v.meta)
+    //         })
+
+    //         this.breaks = arr.join("/")
+    //     }
+    // },
+
+    //computed里面的函数监听到函数中引用的所有实例下的属性的变化
+    computed:{
+        breaks(){
+            const {matched} = this.$route;
+            const arr = [];
+            
+            matched.forEach(v => {
+                arr.push(v.meta)
+            })
+
+            return arr.join("/");
         }
     },
     mounted(){
